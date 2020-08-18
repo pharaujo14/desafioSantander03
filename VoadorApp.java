ca07c1f2d915e8b208b426470e4d04a1a05b1b03
paulo.silva <pharaujo14@gmail.com>
2020-08-18 18:30:02 -0300

    Ajustes de identação

--- VoadorApp.java
+++ VoadorApp.java
@@ -2,10 +2,12 @@ package desafioSantander03;
 
 public class VoadorApp {
 	public static void main(String[] args) {
+		
 		Pato pato = new Pato();
 		Aviao aviao = new Aviao();
 		SuperHomem superman = new SuperHomem();
 		TorreDeControle tc = new TorreDeControle(pato, aviao, superman);
+		
 		tc.voemTodos();
 		System.out.println("------------------------------------");
 		tc.voemTodos();
